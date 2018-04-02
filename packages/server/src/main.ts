3afba6e027f1eb320dee0d2c9bb89eec8c8de696
Silvelo <arturo.silvelo@gmail.com>
2018-04-02 06:56:00 +0200

    lerna

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import './config';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
+import { join } from 'path';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(cors());
-  await app.listen(process.env.PORT);
+  const server = express();
+  const app = await NestFactory.create(AppModule, server, { cors: true, bodyParser: true });
+  app.setGlobalPrefix('api');
+  app.use(express.static(join(__dirname, 'client')));
+  await app.listen(parseInt(process.env.PORT, 10));
+
+  if (process.env.PROD) server.get('*', notFound);
+
+  console.log(`Server start at por ${process.env.PORT}`);
 }
+
+function notFound(req, res) {
+  res.sendFile(join(__dirname, 'client/index.html'));
+}
+
 bootstrap();
